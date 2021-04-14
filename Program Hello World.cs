1f1f30c0194e599afa1b5915d34a2c7ac120146d
BuligaIgor <82317737+BuligaIgor@users.noreply.github.com>
2021-04-14 21:13:41 +0300

    Add files via upload

---
+++ Program Hello World.cs
@@ -0,0 +1,13 @@
+﻿class CPU
+{
+    public string Number, Segment;
+    public int Core, Year;
+    public double BFrequency, TFrequency;
+    public bool HyperT;
+
+    public int GetNumberOfThreads()
+    {
+        if (HyperT == true) return (Core * 2);
+        else return (Core);
+    }
+}
